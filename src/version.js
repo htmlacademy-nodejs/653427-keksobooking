206816fdfc71ace52094edcfbb90f6d7a9571bf5
arsenijnikitockin <arseniynikitochkin@ya.ru>
2018-10-02 01:34:23 +0300

    Раскрасил консоль

--- src/version.js
+++ src/version.js
@@ -1,11 +1,15 @@
 'use strict';
 
+require(`colors`);
+
 const packageInfo = require(`../package.json`);
+
+const palette = [`red`, `green`, `blue`];
 
 module.exports = {
   name: `version`,
   description: `показывает версию проекта`,
   execute() {
-    console.log(`v${packageInfo.version}`);
+    console.log(`v${packageInfo.version.split(`.`).map((version, i) => version[palette[i]]).join(`.`)}`);
   }
 };
