6d316736274f33200146ec3b8dcf6595ea92ea97
arsenijnikitockin <arseniynikitochkin@ya.ru>
2018-10-01 18:03:33 +0300

    команда help через map

--- src/help.js
+++ src/help.js
@@ -5,15 +5,14 @@ const author = require(`./author`);
 const license = require(`./license`);
 const description = require(`./description`);
 
+const optionModules = [version, author, license, description];
+
 module.exports = {
   name: `help`,
   description: `Показывает доступные команды`,
   execute() {
     console.log(`Доступные команды:
 --help    — печатает этот текст;
---${author.name}    — ${author.description};
---${license.name}    — ${license.description};
---${description.name}    — ${description.description};
---${version.name} — ${version.description};`);
+${optionModules.map((module) => `--${module.name}  -${module.description}`).join(`\n`)}`);
   }
 };
